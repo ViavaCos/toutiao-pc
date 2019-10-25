432893f890625e4b442d0f39cfda8b8c0dd5f464
ViavaCos <13811092694@163.com>
2019-10-25 21:23:21 +0800

    添加表单验证规则

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,30 @@
   <div class="container">
     <el-card class="box-card">
       <img src="../../assets/logo_index.png" alt />
-      <el-form ref="form">
-        <el-form-item>
-          <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
+      <el-form ref="form" :model="form">
+        <el-form-item
+          prop="mobile"
+          :rules="[
+      { required: true, message: '手机号不能为空'},
+      { type: 'number', message: '手机号必须为数字值'}
+    ]"
+        >
+          <el-input
+            type="mobile"
+            v-model.number="form.mobile"
+            placeholder="请输入手机号"
+            autocomplete="off"
+          ></el-input>
         </el-form-item>
 
-        <el-form-item>
-          <el-input v-model="form.code" class="code" placeholder="请输入验证码"></el-input>
+        <el-form-item
+          prop="code"
+          :rules="[
+      { required: true, message: '验证码不能为空'},
+      { type: 'number', message: '验证码必须为数字值'}
+    ]"
+        >
+          <el-input v-model.number="form.code" class="code" placeholder="请输入验证码"></el-input>
           <el-button>发送验证码</el-button>
         </el-form-item>
 
