0b873b4e7b5424c7b2e90fc6f9d468c871d6b16e
ViavaCos <13811092694@163.com>
2019-10-26 16:01:42 +0800

    基础登录功能实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
   <div class="container">
     <el-card class="box-card">
       <img src="../../assets/logo_index.png" alt />
-      <el-form ref="form">
-        <el-form-item>
+      <el-form ref="loginForm" :rules="rules" :model="form" status-icon>
+        <el-form-item prop="mobile">
           <el-input v-model="form.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
 
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input v-model="form.code" class="code" placeholder="请输入验证码"></el-input>
-          <el-button>发送验证码</el-button>
+          <el-button >发送验证码</el-button>
         </el-form-item>
 
-        <el-checkbox class="readRule" :value="true" label="我已阅读并同意用户协议和隐私条款"></el-checkbox>
+        <el-checkbox class="readRule" v-model="isChecked" label="我已阅读并同意用户协议和隐私条款"></el-checkbox>
       </el-form>
-      <el-button type="primary" class="login">立即登录</el-button>
+      <el-button :disabled="!isChecked" type="primary" class="login" @click="login('loginForm')">立即登录</el-button>
     </el-card>
   </div>
 </template>
@@ -22,11 +22,57 @@
 <script>
 export default {
   data () {
+    const checkLogin = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
+
+    // const sendCode = (rule, value, callback) => {
+    //   this.$refs.ruleForm.validateField('mobile')
+    // }
+
     return {
       form: {
         mobile: '',
         code: ''
-      }
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkLogin, trigger: 'blur' }
+        ],
+
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码为6位', trigger: 'blur' }
+
+        ]
+      },
+      isChecked: false
+    }
+  },
+  methods: {
+
+    // 登录
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$axios({
+            url: 'authorizations',
+            method: 'post',
+            data: this.form
+          }).then(() => {
+            this.$router.push('/')
+          }).catch(() => {
+            this.$message.error('登录失败, 请重试！')
+          })
+        } else {
+          // alert('登录失败')
+        }
+      })
     }
   }
 }
