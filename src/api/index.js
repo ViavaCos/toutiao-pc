a5eedaab47025d5a55bfad7d9ae0cf45295a22fe
ViavaCos <13811092694@163.com>
2019-10-31 19:39:17 +0800

    内容管理 布局&功能完善

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,20 @@ import axios from 'axios'
 // 导入session工具
 import session from '@/utils/session'
 import router from '../router'
+// 引入json-bigint处理最大安全数问题
+import JSONBIG from 'json-bigint'
 
 // 设置基准地址
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// 处理js中使用JSON.parse转换超出最大数产生偏差的问题
+axios.defaults.transformResponse = [(data) => {
+  try {
+    return JSONBIG.parse(data)
+  } catch (error) {
+    return data
+  }
+}]
+
 // 设置默认请求头   一旦登录页刷新，这里直接凉凉   刷新 ===  执行关于Vue的所有代码
 // let data = session.getToken() ? session.getToken() : {}
 
@@ -22,7 +33,7 @@ axios.interceptors.request.use(config => {
 
 // 如果发送的token失效的话，则需要使用 响应拦截器 来解决
 axios.interceptors.response.use(config => config, error => {
-//   console.dir(error)
+  //   console.dir(error)
   if (error.response.status === 401) {
     // window.location.hash = '#/login'
     return router.push('/login')
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -12,12 +12,12 @@
             <el-radio :label="1">待审核</el-radio>
             <el-radio :label="2">审核通过</el-radio>
             <el-radio :label="3">审核失败</el-radio>
-            <el-radio :label="4">已删除</el-radio>
+            <!-- <el-radio :label="4">已删除</el-radio> -->
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-          <el-select v-model="queryDate.channel_id" placeholder="请选择">
-            <el-option v-for="item in channel" :key="item.id" :label="item.label" :value="item.id"></el-option>
+          <el-select v-model="queryDate.channel_id" placeholder="请选择" clearable>
+            <el-option v-for="item in channel" :key="item.id" :label="item.name" :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="日期">
@@ -27,33 +27,57 @@
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
+            value-format="yyyy-MM-dd"
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" size="small">筛选</el-button>
+          <el-button type="primary" size="small" @click="getArticles">筛选</el-button>
         </el-form-item>
       </el-form>
     </el-card>
     <el-card class="box-card result">
-      <div slot="header">
-        根据筛选条件共查询到
-        <span>{{total_count}}</span> 条结果：
-      </div>
+      <div slot="header">根据筛选条件共查询到 {{total_count}} 条结果：</div>
       <el-table :data="tableData" style="width: 100%">
         <el-table-column label="封面">
-          <img src="../../assets/error.gif" alt />
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" style="height:100px;width:150px;" fit="fill">
+              <div slot="error">
+                <img src="../../assets/error.gif" height="100" width="150" />
+              </div>
+            </el-image>
+          </template>
         </el-table-column>
         <el-table-column prop="title" label="标题"></el-table-column>
-        <el-table-column prop="status" label="状态">
-          <el-tag type="success">审核通过</el-tag>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 1">待审核</el-tag>
+            <el-tag v-if="scope.row.status === 2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 3" type="warning">审核失败</el-tag>
+            <!-- <el-tag v-if="scope.row.status === 4" type="danger">已删除</el-tag> -->
+          </template>
         </el-table-column>
         <el-table-column prop="pubdate" label="发布时间"></el-table-column>
-        <el-table-column prop="action" label="操作">
-          <el-button type="primary" icon="el-icon-edit" circle></el-button>
-          <el-button type="danger" icon="el-icon-delete" circle></el-button>
+        <el-table-column label="操作" width="140">
+          <template slot-scope="scope">
+            <el-button
+              @click="toEdit(scope.row.id)"
+              type="primary"
+              icon="el-icon-edit"
+              plain
+              circle
+            ></el-button>
+            <el-button @click="del(scope.row.id)" type="danger" icon="el-icon-delete" plain circle></el-button>
+          </template>
         </el-table-column>
       </el-table>
-      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      <el-pagination
+        @current-change="changePage"
+        background
+        layout="prev, pager, next"
+        :page-size="queryDate.per_page"
+        :total="total_count"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -66,35 +90,97 @@ export default {
         status: null,
         channel_id: null,
         begin_pubdate: null,
-        end_pubdate: null
+        end_pubdate: null,
+        page: 1,
+        per_page: 20
       },
       channel: [{ label: 'Java', id: 0 }, { label: 'JavaScript', id: 1 }],
       //   data: [begin_pubdate, end_pubdate]
       data: [],
-      total_count: 58619,
-      tableData: [
-        {
-          images: '../../assets/logo_admin.png',
-          title: '国庆八天假',
-          status: '审核通过',
-          pubdate: '2019-09-22',
-          action: 'no'
-        },
-        {
-          images: '../../assets/logo_admin.png',
-          title: '国庆八天假',
-          status: '审核通过',
-          pubdate: '2019-09-22',
-          action: 'no'
-        },
-        {
-          images: '../../assets/logo_admin.png',
-          title: '国庆八天假',
-          status: '审核通过',
-          pubdate: '2019-09-22',
-          action: 'no'
-        }
-      ]
+      total_count: 0,
+      tableData: []
+    }
+  },
+  created () {
+    this.getArticles()
+    this.getChannels()
+  },
+  methods: {
+    // 获取文章列表
+    async getArticles () {
+      // 判断当前频道是否为空字符串，是则赋值为null
+      if (this.queryDate.channel_id === '') {
+        this.queryDate.channel_id = null
+      }
+
+      if (this.data) {
+        this.queryDate.begin_pubdate = this.data[0]
+        this.queryDate.end_pubdate = this.data[1]
+      } else {
+        this.queryDate.begin_pubdate = null
+        this.queryDate.end_pubdate = null
+      }
+
+      const {
+        data: { data }
+      } = await this.$axios({
+        url: 'articles',
+        params: this.queryDate
+      })
+      this.tableData = data.results
+      this.total_count = data.total_count
+    },
+    // 获取频道
+    async getChannels () {
+      const {
+        data: { data }
+      } = await this.$axios({
+        url: 'channels'
+      })
+
+      this.channel = data.channels
+    },
+    // 页面切换
+    changePage (id) {
+      this.queryDate.page = id
+      // console.log(id)
+      this.getArticles()
+    },
+    // 编辑
+    toEdit (id) {
+      this.$router.push({ path: '/publish', query: { id } })
+    },
+    // 删除
+    del (id) {
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.just_del(id)
+        })
+        .catch(() => {
+          this.$message('已取消删除')
+        })
+    },
+    async just_del (id) {
+      try {
+        await this.$axios({
+          method: 'DELETE',
+          url: `articles/${id}`
+        })
+        this.$message({
+          message: '删除成功',
+          type: 'success'
+        })
+      } catch (e) {
+        this.$message({
+          message: '删除失败',
+          type: 'error'
+        })
+      }
+      this.getArticles()
     }
   }
 }
@@ -103,16 +189,6 @@ export default {
 <style scoped lang="less">
 .el-card.result {
   margin-top: 20px;
-
-  .el-table {
-    .el-table-column {
-      width: 20%;
-    }
-
-    img {
-      height: 100px;
-    }
-  }
 
   .el-pagination {
     text-align: center;
